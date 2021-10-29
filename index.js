49f8684bfb3110998d9d87e79d420fc34f4114ec
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-10-29 18:43:02 -0300

    update games is complete

--- index.js
+++ index.js
@@ -116,6 +116,46 @@ app.delete("/game/:id",(req,res) =>{
 
 })
 
+
+app.put("/game/:id",(req,res)=>{ // update
+    if(isNaN(req.params.id)){
+        res.sendStatus(400) //not validator
+
+    }else{
+        var id  = parseInt(req.params.id)
+        var game = DB.games.find(g => g.id == id)
+
+        if(game != undefined){
+            
+            var{ title, price, year} = req.body;
+            if( title != undefined){
+                game.title = title;
+
+            }
+            if( price != undefined){
+                if(isNaN(price)){
+                    res.sendStatus(400)
+                }else{
+                game.price = price;
+                }
+            }
+            if( year != undefined){
+             if(isNaN(year)){
+                res.sendStatus(400)
+             }else{
+                game.year= year;
+             }
+            }
+         res.sendStatus(200)   
+
+
+        }else{
+            res.sendStatus(404) // Not found
+        }
+    }
+    
+
+})
 app.listen(8080,()=>{
         console.log("API RODANDO!")
 })
